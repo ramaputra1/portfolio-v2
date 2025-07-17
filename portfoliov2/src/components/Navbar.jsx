b0fa6ee6c963c5ff4f3eec5c0edbb72a23683933
ramaputra1 <rama.aptra@gmail.com>
2025-07-16 15:51:17 -1000

    navbar done

--- portfoliov2/src/components/Navbar.jsx
+++ portfoliov2/src/components/Navbar.jsx
@@ -20,31 +20,29 @@ const Navbar = () => {
     <div className="navbar py-7 flex items-center justify-between">
       <div className="logo">
         <h1 className="text-3xl font-bold bg-white text-black p-1 md:bg-transparent md:text-white">
-          Rama Portfolio
+          React Training
         </h1>
       </div>
       <ul
-        className={`menu bg-[#000000]/90 p-3 rounded-br-2xl rounded-bl-2xl flex gap-10 items-center fixed left-1/2 -translate-x-1/2 md:-translate-0 -top-10 md:static ${
-          active ? "top-0 opacity-100" : "-top-0 opacity-0"
-        }`}
+        className={`menu flex items-center gap-4 fixed left-1/2 -translate-x-1/2 bg-white/30 backdrop-blur-md p-3 rounded-br-2xl rounded-bl-2xl transition-all sm:gap-10 md:static md:-translate-x-0 md:opacity-100 md:bg-transparent md:transition-none ${active ? "top-0 opacity-100" : "-top-10 opacity-0"}`}
       >
         <li>
-          <a href="#" className="text-lg font-medium">
+          <a href="#" className="text-base font-medium">
             Home
           </a>
         </li>
         <li>
-          <a href="#" className="text-lg font-medium">
+          <a href="#" className="text-base font-medium">
             About
           </a>
         </li>
         <li>
-          <a href="#" className="text-lg font-medium">
+          <a href="#" className="text-base font-medium">
             Project
           </a>
         </li>
         <li>
-          <a href="#" className="text-lg font-medium">
+          <a href="#" className="text-base font-medium">
             Contact
           </a>
         </li>
