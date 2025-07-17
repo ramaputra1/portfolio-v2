267898b170b761e0544397d694b8f9b5beff2ba2
ramaputra1 <rama.aptra@gmail.com>
2025-07-16 16:59:54 -1000

    Hero

--- portfoliov2/src/App.jsx
+++ portfoliov2/src/App.jsx
@@ -1,88 +1,26 @@
+import DataImage from "./data"
+
 function App() {
   return (
     <>
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut,
-        aspernatur? Porro impedit quaerat reiciendis, unde debitis et facere
-        natus harum iusto vitae corrupti nam iure nostrum repudiandae?
-        Accusantium, ipsa saepe? Facere placeat fuga, blanditiis aspernatur
-        voluptatibus dolore saepe necessitatibus animi assumenda cum eos
-        dolorem. Vero provident earum ab autem, error doloremque sapiente,
-        consequuntur molestiae nesciunt non ea minima tempora voluptatum! Ipsum
-        repudiandae at quidem corporis voluptate eos vel! Quae, libero?
-        Architecto voluptatem perspiciatis reprehenderit delectus ut dignissimos
-        sint enim facilis tempore consequatur. Necessitatibus dolorem iste
-        repellendus dolorum corporis obcaecati omnis! Ut repellat libero
-        asperiores voluptas. Illum deserunt repudiandae ea nihil harum nobis ab
-        at iusto quae ratione exercitationem id cum nam suscipit facilis autem,
-        voluptates quos. Cumque repudiandae quaerat incidunt. Illum voluptatibus
-        cumque rerum placeat saepe quod doloribus facilis nihil assumenda
-        eligendi quos aspernatur accusamus, nostrum ipsam consequuntur autem?
-        Repellat illo saepe labore ea eius accusantium iste officia assumenda
-        quae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-        quidem exercitationem delectus eos officiis ducimus asperiores unde cum
-        nostrum accusamus odit illo incidunt quas quae, eveniet, temporibus at
-        fuga rerum. Esse necessitatibus totam deleniti cum explicabo molestiae.
-        Iusto fugit error eius ipsam totam in, quas harum ipsa dicta pariatur
-        est repudiandae vero inventore nobis aut maiores aliquam molestias velit
-        quis. Itaque, libero tempora ipsam a hic magnam. Suscipit ea similique
-        sed accusantium voluptatibus, numquam maiores aliquid eveniet adipisci,
-        perspiciatis dicta? Quasi, in at quam odio repellendus facere nemo
-        sapiente. Ratione? Rerum sequi, atque nemo necessitatibus, doloribus
-        ipsum voluptatem saepe unde qui praesentium, omnis tempore velit ipsam!
-        Iste et libero, earum nostrum harum labore iure impedit, enim cupiditate
-        a, quisquam rerum. Iure neque aliquid minima fugiat voluptatum autem
-        tenetur repudiandae omnis sequi placeat sit, officiis pariatur excepturi
-        enim velit culpa ad nisi! Officiis iste et mollitia suscipit error omnis
-        voluptas dolorum? Tenetur cupiditate nobis ad dolores, similique
-        officiis debitis aliquam suscipit rem reiciendis deserunt corporis amet.
-        Quisquam vitae harum amet totam ullam a quaerat inventore, quasi
-        repellat sit consequatur maiores consequuntur. Excepturi quis saepe a
-        labore suscipit voluptates praesentium, quam, nam beatae adipisci sed
-        earum quibusdam ullam deleniti laborum? Autem mollitia ratione culpa
-        recusandae vitae incidunt labore fugiat architecto veniam nulla. Enim
-        eum eius repellat ad fugit commodi atque aliquid magni, consequatur sed
-        cupiditate accusamus ipsum et libero soluta quis accusantium animi! Quia
-        dicta aperiam atque nisi nam. Modi, architecto repellat. Blanditiis sit
-        nostrum laudantium ipsa ipsum, explicabo dolores doloremque nobis? Culpa
-        obcaecati amet voluptatem non esse praesentium deleniti in similique
-        sunt eveniet inventore modi, accusamus dignissimos recusandae alias quo
-        impedit. Culpa cum obcaecati, explicabo repudiandae voluptatem, vitae
-        reiciendis voluptas vero repellat ea a error minima corporis autem nisi
-        dolore. Id iusto dolorum quia quos ratione in distinctio cupiditate
-        dolore alias. Cumque dolore ad in soluta illum officia fugiat vel
-        distinctio cum molestias non eligendi atque quod veniam repellat laborum
-        harum, eveniet nam a ducimus laboriosam. Praesentium error pariatur
-        tempora saepe. Quod expedita hic, voluptate distinctio impedit ullam eum
-        aliquam illum consequatur deleniti quibusdam at fugiat accusantium harum
-        dolor porro necessitatibus corporis minus ab natus ipsa ut modi,
-        corrupti reiciendis. Quia? Animi ab architecto similique eum maxime
-        assumenda temporibus nam est laudantium quibusdam! Eum, minus non iure,
-        minima ad est dolorum dolores tenetur nemo quos, velit veritatis
-        deserunt amet. Voluptatem, a? Ab odio eum quia rem minus magni libero,
-        maxime, iusto voluptatum velit explicabo vero architecto minima
-        repudiandae veritatis, distinctio alias eos a quaerat! Veniam molestias,
-        corrupti at esse commodi possimus. Vitae eius eum modi dolores cum omnis
-        dolor velit minima labore quod. Eum magni ea labore quaerat quas
-        deserunt corrupti, eligendi fugit dolorem, alias vero. Velit dicta
-        aperiam ex ullam? Sed dicta et quos, adipisci quae explicabo facilis
-        commodi error omnis id culpa amet harum nisi, expedita sunt ratione
-        vitae quod qui consequatur, placeat possimus. Totam sequi neque magni
-        laboriosam. Aliquam pariatur velit quibusdam distinctio! Id quaerat,
-        perspiciatis culpa reiciendis amet qui incidunt officiis. Repudiandae
-        tenetur quod voluptate doloribus, explicabo rerum necessitatibus
-        eligendi provident quis voluptates, unde dolorem dignissimos distinctio.
-        Ratione, aliquid quidem! Nulla incidunt fugiat molestiae odio quisquam
-        tenetur cupiditate eum, libero aut explicabo totam, reprehenderit veniam
-        labore eaque porro vel eveniet fuga iure similique voluptatum facere
-        illum blanditiis. Distinctio maxime totam ab non molestiae? Nihil dolor,
-        temporibus est minima amet possimus quibusdam mollitia consectetur
-        fugiat. Reiciendis inventore facere, laudantium, iusto dolores
-        cupiditate odit non voluptates tenetur, a porro. Alias laborum veniam
-        cum neque, consequatur nostrum repellat ipsam possimus repellendus vero
-        nobis earum non fuga atque, iure recusandae quam. Quidem totam deleniti
-        a ducimus aliquam adipisci eaque in magni.
-      </h1>
+     <div className="hero grid pt-15 grid-cols-1 md:grid-cols-2 lg:gap-10">
+      <div>
+        <div className="flex items-center gap-4 bg-zinc-800/50 w-fit p-4 rounded-2xl mb-5">
+          <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md"/>
+          <q>Lorem ipsum dolor sit amet.</q>
+        </div>
+        <h1 className="text-5xl/tight font-bold mb-5">Lorem, ipsum dolor.</h1>
+        <p className="text-base/loose mb-6 opacity-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, natus explicabo tenetur facere aperiam minima consequatur iusto officia corrupti distinctio a voluptatibus quibusdam doloremque, quas maiores doloribus, neque id placeat!
+        Enim nobis eveniet commodi, provident error reiciendis nihil? Exercitationem vel, quo eaque eius ipsa dolorum sapiente excepturi error fugit soluta, aperiam harum numquam beatae perferendis, eum cumque iste. Quaerat, aut!
+        Molestiae itaque soluta sed sit, facere neque atque omnis harum doloribus amet tempore cum veritatis consequatur alias provident ipsa minus! Facilis sapiente aliquid quis animi corporis voluptates odit in a!
+        </p>
+        <div className="flex items-center gap-4">
+          <a href="#" className="bg-violet-700/50 p-4 rounded-2xl hover:bg-violet-500/50">Download <i className="ri-download-line ri-lg"></i></a>
+          <a href="#" className="bg-zinc-700/50 p-4 rounded-2xl hover:bg-zinc-500/50">Project <i className="ri-arrow-down-circle-line ri-lg"></i></a>
+        </div>
+      </div>
+      <img src={DataImage.HeroImage} alt="Hero Image" className="w-[500px] mt-10 md:mt-0 ml-auto"/>
+     </div>
     </>
   );
 }
