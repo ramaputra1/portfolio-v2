51f0b1088fa27d47133f37cc1fd132babfce7825
ramaputra1 <rama.aptra@gmail.com>
2026-06-12 02:36:55 -1000

    ui-components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,6 @@
+export { Button } from "./Button";
+export { Card } from "./Card";
+export { IconLink } from "./IconLink";
+export { Section } from "./Section";
+export { SectionHeader } from "./SectionHeader";
+export { TechTag } from "./TechTag";
